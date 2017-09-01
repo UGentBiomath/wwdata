a6a2e7ff10aa4e22d70629f573c1e4d0556fae66
cpdmulde <chaim.demulder@ugent.be>
2017-09-01 16:30:18 +0200

    add requirements file

--- setup.py
+++ setup.py
@@ -12,7 +12,12 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    'pandas'
+    'numpy'
+    'dateutils'
+    'scipy'
+    'matplotlib'
+    'statsmodels'
 ]
 
 setup_requirements = [
