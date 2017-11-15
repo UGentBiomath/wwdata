6e18885113fabbbb0ad2271e51a29f9b9625f27d
cpdmulde <chaim.demulder@ugent.be>
2017-11-15 22:45:25 +0100

    add custom stylesheets and make sure conf.py finds them

--- docs/conf.py
+++ docs/conf.py
@@ -145,6 +145,7 @@ html_theme = 'default'
 # static files, so a file named "default.css" will overwrite the builtin
 # "default.css".
 # html_static_path = ['_static']
+html_static_path = ['custom_stylesheets']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
